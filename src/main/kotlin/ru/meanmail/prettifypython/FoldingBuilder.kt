e506ddd8aa70241736fd2fa808ff1afdc049fe38
GiilDe <38991777+gilad212387@users.noreply.github.com>
2020-04-28 20:28:51 +0300

    Adding features for easier scalability: loading of conversions from JSON file rather than hard-coding them. Adding JSON file for each type of conversions (existed - basic conversions, added now - greek letter conversions, and more to follow) to allow easier allowing or cancelling of conversion of some type. Condition functions that allow or cancel conversion are now loaded from hash map.

--- src/main/kotlin/ru/meanmail/prettifypython/FoldingBuilder.kt
+++ src/main/kotlin/ru/meanmail/prettifypython/FoldingBuilder.kt
@@ -9,28 +9,61 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.impl.source.tree.LeafPsiElement
 import com.jetbrains.python.psi.PyBinaryExpression
 import com.jetbrains.python.psi.PyStringElement
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.*
+import kotlinx.serialization.builtins.list
+import java.io.File
 
 
 class PrettifyFoldingBuilder : FoldingBuilder {
 
-    private val prettySymbolMaps = hashMapOf<String, (PsiElement) -> String?>(
-            ">=" to { _: PsiElement -> "≥" },
-            "<=" to { _: PsiElement -> "≤" },
-            "!=" to { _: PsiElement -> "≠" },
-            "->" to { _: PsiElement -> "➔" },
-            "lambda" to { _: PsiElement -> "λ" },
-            "**" to { node: PsiElement ->
-                if (node.parent is PyBinaryExpression) "^" else null
+    companion object {
+
+        private fun initializePrettySymbolMaps(): HashMap<String, (PsiElement) -> String?>{
+
+             fun initializeFile(path: String,
+                                conditions: HashMap<ConversionCondition, (node: PsiElement) -> (Boolean)>,
+                                prettySymbolMaps: HashMap<String, (PsiElement) -> String?>,
+                                conversionListSerializer: KSerializer<List<Conversion>>) {
+
+                val conversions = Json.parse(conversionListSerializer, File(path).readText())
+                for (conversion in conversions) {
+                    if (conversion.cond == ConversionCondition.NONE) {
+                        prettySymbolMaps[conversion.source] = { _: PsiElement -> conversion.dest }
+                    } else {
+                        val condition = conditions[conversion.cond]
+                        prettySymbolMaps[conversion.source] =
+                                { node: PsiElement -> if ((condition!!)(node.parent)) conversion.dest else null }
+                    }
+                }
             }
-    )
+
+            val prettySymbolMaps = hashMapOf<String, (PsiElement) -> String?>()
+
+            val prefix = "src/main/kotlin/ru/meanmail/prettifypython/"
+            val BasicConversions = "${prefix}BasicConversions.json"
+            val GreekLetters = "${prefix}GreekLetters.json"
+
+            val isPyBinExp = { node: PsiElement -> node.parent is PyBinaryExpression }
+            val conditions = hashMapOf<ConversionCondition, (node: PsiElement) -> (Boolean)>(
+                    ConversionCondition.PY_BINARY_EXP to isPyBinExp
+            )
+
+            val conversionListSerializer: KSerializer<List<Conversion>> = Conversion.serializer().list
+
+            initializeFile(BasicConversions, conditions, prettySymbolMaps, conversionListSerializer)
+            initializeFile(GreekLetters, conditions, prettySymbolMaps, conversionListSerializer)
+            return prettySymbolMaps
+        }
+
+        val prettySymbolMaps = initializePrettySymbolMaps()
+    }
 
     private fun getDescriptorsForChildren(node: ASTNode): List<FoldingDescriptor> {
         val descriptors = mutableListOf<FoldingDescriptor>()
-
         for (child in node.getChildren(null)) {
             descriptors.addAll(getDescriptors(child))
         }
-
         return descriptors
     }
 
