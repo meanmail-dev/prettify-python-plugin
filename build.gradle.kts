c86c2cf7893b89058244c0262559a21fcab566e4
meanmail <meanmail@meanmail.ru>
2021-04-11 18:27:42 +0700

    Update build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.4.10"
-    id("org.jetbrains.intellij") version "0.6.3"
+    kotlin("jvm") version "1.4.30"
+    id("org.jetbrains.intellij") version "0.7.2"
 }
 
 group = "ru.meanmail"
@@ -16,7 +16,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    testImplementation("junit:junit:4.13")
+    testImplementation("junit:junit:4.13.2")
 }
 
 configure<JavaPluginConvention> {
@@ -45,7 +45,12 @@ intellij {
     } else {
         project.properties["IdeVersion"].toString()
     }
-    setPlugins(project.properties["pythonPluginVersion"].toString())
+    type = project.properties["ideType"].toString()
+    if (type !in setOf("PY", "PC")) {
+        setPlugins(project.properties["pythonPluginVersion"].toString())
+    } else {
+        setPlugins("PythonCore")
+    }
 }
 
 fun readChangeNotes(pathname: String): String {
@@ -69,8 +74,11 @@ fun readChangeNotes(pathname: String): String {
         }
     }
 
-    return notes.joinToString("</p><br><p>", prefix = "<p>",
-            postfix = "</p><br>") {
+    return notes.joinToString(
+        "</p><br><p>",
+        prefix = "<p>",
+        postfix = "</p><br>"
+    ) {
         it.joinToString("<br>")
     } +
             "See the full change notes on the <a href='" +
